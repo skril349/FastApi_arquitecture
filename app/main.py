435ce0ade5fa7e11ea38fc19474f82126ff5e999
skril349 <tvives.cabaleiro@gmail.com>
2025-12-17 11:44:44 +0100

    getting media files

--- app/main.py
+++ app/main.py
@@ -4,7 +4,13 @@ from app.core.db import Base, engine
 from app.api.v1.posts.router import router as post_router
 from app.api.v1.auth.router import router as auth_router
 from app.api.v1.uploads.router import router as upload_router
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+import os
 load_dotenv()
+MEDIA_DIR = Path("app") / "media"
+
+
 
 def create_app() -> FastAPI:
     app = FastAPI(title="Mini Blog")
@@ -12,6 +18,10 @@ def create_app() -> FastAPI:
     app.include_router(auth_router, prefix="/api/v1")
     app.include_router(post_router)
     app.include_router(upload_router)
+    
+    os.makedirs(MEDIA_DIR, exist_ok=True)
+    app.mount("/media", StaticFiles(directory=MEDIA_DIR), name="media")
+    
     return app
 
 app = create_app()
