b4fe2bf12013d8f31dd96368a559eed34a601b63
skril349 <tvives.cabaleiro@gmail.com>
2025-12-18 17:16:53 +0100

    list tags

--- app/api/v1/tags/repository.py
+++ app/api/v1/tags/repository.py
@@ -1,16 +1,52 @@
 
 # Consultas a bases de datos relacionados con tags
 
+from typing import Optional
 from sqlalchemy import func, select
 from sqlalchemy.orm import Session
 
+from app.api.v1.tags.schemas import TagPublic
 from app.models.tag import TagORM
+from app.services.pagination import paginate_query
 
 
 class TagRepository:
     def __init__(self, db:Session):
         self.db = db
         
+    
+    def list_tags(
+        self,
+        search:Optional[str] = None,
+        order_by:str = "id",
+        direction:str = "asc",
+        page:int = 1,
+        per_page:int = 10) :
+        query = select(TagORM)
+        if search:
+            query = query.where(func.lower(TagORM.name).ilike(f"%{search.lower()}%"))
+        
+        allowed_order = {
+            "id": TagORM.id,
+            "name":func.lower(TagORM.name)
+        }
+    
+        result = paginate_query(
+            db = self.db,
+            model = TagORM,
+            base_query = query,
+            page= page,
+            per_page=per_page,
+            order_by =order_by,
+            direction=direction,
+            allowed_order=allowed_order
+            
+        )
+        
+        result["items"] = [TagPublic.model_validate(item) for item in result["items"]]
+        return result
+    
+    
     def create_tag(self, tag_name:str):
         normalize = tag_name.strip().lower()
         tag_obj = self.db.execute(
--- app/api/v1/tags/router.py
+++ app/api/v1/tags/router.py
@@ -1,6 +1,7 @@
 
-from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi import APIRouter, Depends, HTTPException, Query, status
 
+from app.api.v1.tags import repository
 from app.api.v1.tags.repository import TagRepository
 from app.api.v1.tags.schemas import TagCreate, TagPublic
 from app.core.db import get_db
@@ -9,6 +10,19 @@ from sqlalchemy.exc import SQLAlchemyError
 from app.core.security import get_current_user
 
 router = APIRouter(prefix="/tags", tags=["tags"])
+
+
+@router.get("",response_model=dict)
+def list_tags(
+    page: int = Query(1, ge=1),
+    per_page:int =Query(10,ge = 1, le = 100),
+    order_by: str = Query("id", pattern="^(id|name)$"),
+    direction: str = Query("asc",pattern="^(asc|desc)$"),
+    search: str | None = Query(None),
+    db:Session = Depends(get_db)
+):
+    repository = TagRepository(db)
+    return repository.list_tags(page = page,per_page=per_page,order_by=order_by,direction=direction,search=search)
 
 @router.post("",response_model=TagPublic, response_description="post creado", status_code=status.HTTP_201_CREATED)
 def create_tag(tag:TagCreate, db:Session = Depends(get_db), user = Depends(get_current_user)):
