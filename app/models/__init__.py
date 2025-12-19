b25b1c10e19fe57f9c4143bdaa6268a2209812f8
skril349 <tvives.cabaleiro@gmail.com>
2025-12-19 12:54:40 +0100

    creating User Model

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,5 +1,6 @@
 from .author import AuthorORM
 from .post import PostORM
 from .tag import TagORM, post_tags
+from .user import UserORM
 
-__all__ = ["AuthorORM", "PostORM", "TagORM", "post_tags"]
+__all__ = ["AuthorORM", "PostORM", "TagORM", "post_tags", "UserORM"]
---
+++ app/models/user.py
@@ -0,0 +1,19 @@
+
+
+from datetime import date, datetime
+from typing import Literal
+from app.core.db import Base
+from sqlalchemy import Boolean, DateTime, Enum, Integer, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+Role = Literal["user", "editor", "admin"]
+
+class UserORM(Base):
+    __tablename__ = "users"
+    id: Mapped[int] = mapped_column(primary_key=True, index = True)
+    email: Mapped[str] = mapped_column(String(255),unique=True, index=True, nullable=False)
+    hashed_password: Mapped[str] = mapped_column(String(255), nullable=False)
+    full_name: Mapped[str] = mapped_column(String(255))
+    role: Mapped[Role] = mapped_column(Enum("user", "editor", "admin", name = "role_enum"),default="user")
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime,default=datetime.utcnow)
