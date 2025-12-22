45a45c87710902cdc3f86f914b983d7789768341
skril349 <tvives.cabaleiro@gmail.com>
2025-12-22 17:01:40 +0100

    preparing seeds files

---
+++ app/seeds/data/users.py
@@ -0,0 +1,40 @@
+
+USERS = [
+    {
+        "email": "admin@example.com",
+        "full_name": "Admin",
+        "password": "Admin123!",
+        "role": "admin"
+    },
+    {
+        "email": "editor@example.com",
+        "full_name": "Editor",
+        "password": "Editor123!",
+        "editor": "editor"
+    },
+    {
+        "email": "user@example.com",
+        "full_name": "User",
+        "password": "User123!",
+        "editor": "user"
+    },
+    {
+        "email": "ricardo@example.com",
+        "full_name": "Ricardo Cuéllar",
+        "password": "pruebas123!",
+        "editor": "admin"
+    },
+    {
+        "email": "fernando@example.com",
+        "full_name": "Fernando Herrera",
+        "password": "pruebas123!",
+        "editor": "editor"
+    },
+    {
+        "email": "juanperez@example.com",
+        "full_name": "Juan Pérez",
+        "password": "pruebas123!",
+        "editor": "user"
+    },
+
+]
